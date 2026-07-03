6d4799f01248d38d8e10c644a2d85a468aa60530
qsi-seg-br <298933843+qsi-seg-br@users.noreply.github.com>
2026-07-03 11:33:53 -0300

    feat: adiciona paginas e logos de marcas

---
+++ _next/static/chunks/app/ferramentas/page-c14ad87f81023782.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[105,863,873],{4744:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,8898,23))}},e=>{e.O(0,[898,480,920,358],()=>e(e.s=4744)),_N_E=e.O()}]);
